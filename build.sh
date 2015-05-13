c1b1f38e5f900b7e38f795f971199ab38a0d467b
Meg Cunningham <meg_cunningham@live.com>
2015-05-13 15:36:56 -0500

    fix build

--- build.sh
+++ build.sh
@@ -9,7 +9,10 @@ cp -r src public
 cd src
 find . -name "*.html" | cpio -pdvm ../public
 cd ..
-rm -rf src/**/*.html public/**/_*html public/_partials
+rm -rf src/*.html \
+       src/**/*.html \
+       public/_partials \
+       public/**/_*html
 
 #compile sass to css
 ./node_modules/.bin/node-sass \
@@ -20,8 +23,13 @@ rm -rf src/**/*.html public/**/_*html public/_partials
 #convert ES6 JS to ES5
 ./node_modules/.bin/babel src --out-dir public -s inline
 
+# concat bower_components to public/lib directory
+if [ -d "bower_components" ]; then
+  ./node_modules/.bin/bowcat . -o public/lib -m
+fi
+
 # clean unneeded files
-rm -rf public_styles \
+rm -rf public/_styles \
        public/*.jade \
        public/**/*.jade \
        public/*.scss \
